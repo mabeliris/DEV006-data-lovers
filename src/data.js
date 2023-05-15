254a688e26dbab51ce123620317b8628de9dca0d
claudiamartinezv <77359482+claudiamartinezv@users.noreply.github.com>
2023-05-15 18:03:39 -0400

    depuración código

--- src/data.js
+++ src/data.js
@@ -1,50 +1,22 @@
-
 //primera historia de usuario, mostrar la data
-
 export const showSports=(array)=> { 
-  const deportes=array.map(athlete =>(athlete.sport));  
-         
+  const deportes=array.map(athlete =>(athlete.sport));          
   return [...new  Set (deportes)]; 
-
 };
 
-
 export const orderSport=()=>{
-
 }
 
-
-
-
-
 //segunda historia, filtrar
-
-/*export const countries= (athletes)=>{
-  const newFilter = athletes.filter((team) => {
-    const result = athletes.team === "USA";
-    return result;
-  });
-  return newFilter;
-};*/
 export const countries= (array)=>{
   const team=array.map(athlete=>(athlete.team));
   return [...new Set(team)]; 
-  
 };
 
-
-
-
 // tercera historia,ordenar 
-
 export const ordenar=()=> {
-
 }
  
-
-  
-
-
 //atletas
 export const deportistas = (array) => {
   const atleta=array.map(athlete=>(athlete.name));
@@ -54,7 +26,6 @@ export const deportistas = (array) => {
 //filtrar paises por atletas
 export const filtrado=(arr,team) =>{
   return arr.filter(el=>el.team===team);
-
 }
 
 
